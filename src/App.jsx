01c7dd81a72ebaa245a56a2ef9dea0be6f246977
Mike Barber <bandofgold1@gmail.com>
2023-10-04 18:47:57 -0400

    updated folder structure and navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Nav from './UI/Nav.jsx';
+import Nav from './components/Nav.jsx';
 
 function App() {
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,4 @@
 // Bringing in the required import from 'react-router-dom'
-import { Link } from 'react-router-dom';
 import Navbar from './UI/Navbar';
 
 export default function Nav() {
@@ -9,9 +8,6 @@ export default function Nav() {
       links={[
         <Link key={1} className="nav-link text-light" to="/">
           Home
-        </Link>,
-        <Link key={2} className="nav-link text-light" to="/about">
-          About Us
         </Link>,
       ]}
     />
