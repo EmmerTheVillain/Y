7d1f5df24c4df05d23a69bbd2a9ab43aa0794ab6
Yajiit <tjagerter@gmail.com>
2023-10-05 18:43:05 -0400

    extended file structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Nav from './UI/Nav.jsx';
+import Home from './pages/Home.jsx'
 
 function App() {
 
   return (
-    <>
-     <Nav />
-    </>
-  )
+    <div className="app">
+      <header>
+        <Nav />
+      </header>
+      <main>
+        <Home />
+      </main>
+      <footer>
+        {/* Add footer here */}
+      </footer>
+    </div>
+  );
 }
 
 export default App
