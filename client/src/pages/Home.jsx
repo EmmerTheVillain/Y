634919bc348d2263c1a97738a0327951ecb34809
Mike Barber <bandofgold1@gmail.com>
2023-10-11 14:07:43 -0400

    set up reactstrap in home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Home() {
   return (
-    <div className="home">
-      <h1>Welcome to y?</h1>
-    </div>
+    <Container>
+      <Row className="home">
+        <Col md={{ span: 6, offset: 3 }}>{
+          <h1>Welcome to y?</h1>
+        }
+        </Col>
+      </Row>
+    </Container>
   );
 }
+
 
 export default Home;
