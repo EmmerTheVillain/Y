7676649f83f7974fdc0f2187cda21e24c7999f56
Mike Barber <bandofgold1@gmail.com>
2023-10-11 18:31:43 -0400

    updated app.jsx to react bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom'; 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import NavOutput from './components/NavOutput.jsx'
 import Home from './pages/Home.jsx';
@@ -11,21 +14,29 @@ import MyTweetPage from './pages/MyTweetPage.jsx';
 function App() {
   return (
     <Router>
-      <div className="app">
-        <header>
-          <NavOutput />
-        </header>
-        <main>
-          <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<AboutUs />} />
-          <Route exact path="/tweet" element={<MyTweetPage />} />
-          </Routes>
-        </main>
-        <footer>
-          {/* Add footer here */}
-        </footer>
-      </div>
+      <Container className="app">
+        <Row>
+          <header>
+            <NavOutput />
+          </header>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col>
+          <main>
+            <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<AboutUs />} />
+            <Route exact path="/tweet" element={<MyTweetPage />} />
+            </Routes>
+          </main>
+          </Col>
+        </Row>
+        <Row>
+          <footer>
+            {/* Add footer here */}
+          </footer>
+        </Row>
+      </Container>
     </Router>
   );
 }
