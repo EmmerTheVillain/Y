a16e30a3f170c7165994e1e327de8ea3d966cda4
Mike Barber <bandofgold1@gmail.com>
2023-10-12 20:17:23 -0400

    added profile component
    added profile component and added it to profile page

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -3,13 +3,14 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Tweet from '../components/Tweet';
+import ProfileComponent from '../components/profileComponent';
 
 function Profile() {
   return (
     <Container>
       <Row className="home">
         <Col md={{ span: 6, offset: 3 }}>{
-          <h1>Welcome to y?</h1>
+          <ProfileComponent />
         }
         </Col>
         <Tweet />
