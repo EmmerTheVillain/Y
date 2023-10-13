1f47d11f217d830373accb698362a73ccd877776
Yajiit <tjagerter@gmail.com>
2023-10-13 11:15:49 -0400

    updated User model

--- server/models/User.js
+++ server/models/User.js
@@ -2,22 +2,40 @@ const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
   username: {
     type: String,
     required: true,
+    lowercase: true,
     unique: true,
     trim: true,
+    match: [/^[0-9a-zA-Z_.-]+$/, 'username must only contain numbers, letters, "." "-" "_"']
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    lowercase: true,
+    trim: true,
     match: [/.+@.+\..+/, 'Must match an email address!'],
   },
   password: {
     type: String,
     required: true,
     minlength: 8,
+    validate: [
+      {
+        validator: function (password) {
+          // Use a regular expression to check for at least one letter and one number
+          return /^(?=.*[A-Za-z])(?=.*\d).+$/.test(password);
+        },
+        message: 'Password must contain at least one letter and one number.',
+      },
+    ],
   },
   tweets: [
     {
